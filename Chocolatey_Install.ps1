b480a154e8f9cb5f38d424f48958b74c13b6f821
rickpro2 <rickie.proctor2@yahoo.com>
2021-04-11 16:25:17 -0400

    Update Chocolatey_Install.ps1

--- Chocolatey_Install.ps1
+++ Chocolatey_Install.ps1
@@ -8,3 +8,59 @@ Break
 
 Write-Host "Installing Chocolatey"
 	Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	choco install chocolatey-core.extension -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+Write-Host "Installing Chocolatey GUI"
+	choco install chocolateygui -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+Write-Host "Installing Google Chrome"
+	choco install googlechrome -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+Write-Host "Installing Brave Browser"
+	choco install brave -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+Write-Host "Installing Microsoft Silverlight"
+	choco install silverlight -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+Write-Host "Installing Adobe Acrobat Reader DC"
+	choco install adobereader -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+Write-Host "Installing VLC media player"
+	choco install vlc -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+Write-Host "Installing Zoom Client for Meetings"
+	choco install zoom -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+Write-Host "Installing Adobe Creative Cloud Client"
+	choco install adobe-creative-cloud -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+Write-Host "Installing TeamViewer"
+	choco install teamviewer9 -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+Write-Host "Installing 7-Zip"
+	choco install 7zip -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+Write-Host "Installing iTunes"
+	choco install itunes -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+Write-Host "Choco Cleaner (Script + Task)"
+	choco install choco-cleaner -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+Write-Host "AllDup"
+	choco install alldup -y
+	$wshell.Popup("Operation Completed",0,"Done",0x0)
+
+choco install camtasia --version=9.05 -y
