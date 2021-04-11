16f363757ae988466c5d5132b794c830343ba1ba
rickpro2 <rickie.proctor2@yahoo.com>
2021-04-11 15:12:56 -0400

    Update and rename Chocolatey_Install_Part1.ps1 to Chocolatey_Install.ps1

---
+++ Chocolatey_Install.ps1
@@ -0,0 +1,10 @@
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+
+Write-Host "Installing Chocolatey"
+	Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
