8e12e6474f7afe3b3522302e6e34cadad9e494b0
rickpro2 <rickie.proctor2@yahoo.com>
2021-04-11 16:38:33 -0400

    Update Chocolatey_Install_Apps.ps1

--- Chocolatey_Install_Apps.ps1
+++ Chocolatey_Install_Apps.ps1
@@ -1,65 +1,16 @@
-choco install choco-upgrade-all-at-startup adobereader googlechrome brave vlc teamviewer zoom itunes 7zip chocolateygui firefox choco-cleaner alldup notepadplusplus -y
+choco install firefox notepadplusplus -y
 
 
 choco install camtasia --version=9.05 -y
 
 
-
-
-
-
-
-
-Write-Host "Installing Chocolatey GUI"
-	choco install chocolateygui -y
-	$wshell.Popup("Operation Completed",0,"Done",0x0)
-
-Write-Host "Installing Google Chrome"
-	choco install googlechrome -y
-	$wshell.Popup("Operation Completed",0,"Done",0x0)
-
-Write-Host "Installing Brave Browser"
-	choco install brave -y
-	$wshell.Popup("Operation Completed",0,"Done",0x0)
-
 Write-Host "Installing Microsoft Silverlight"
 	choco install silverlight -y
 	$wshell.Popup("Operation Completed",0,"Done",0x0)
 
-Write-Host "Installing Adobe Acrobat Reader DC"
-	choco install adobereader -y
-	$wshell.Popup("Operation Completed",0,"Done",0x0)
-
-Write-Host "Installing VLC media player"
-	choco install vlc -y
-	$wshell.Popup("Operation Completed",0,"Done",0x0)
-
-Write-Host "Installing Zoom Client for Meetings"
-	choco install zoom -y
-	$wshell.Popup("Operation Completed",0,"Done",0x0)
 
 Write-Host "Installing Adobe Creative Cloud Client"
-	choco install adobe-creative-cloud -y
-	$wshell.Popup("Operation Completed",0,"Done",0x0)
-
-Write-Host "Installing TeamViewer"
-	choco install teamviewer9 -y
-	$wshell.Popup("Operation Completed",0,"Done",0x0)
-
-Write-Host "Installing 7-Zip"
-	choco install 7zip -y
-	$wshell.Popup("Operation Completed",0,"Done",0x0)
-
-Write-Host "Installing iTunes"
-	choco install itunes -y
-	$wshell.Popup("Operation Completed",0,"Done",0x0)
-
-Write-Host "Choco Cleaner (Script + Task)"
-	choco install choco-cleaner -y
-	$wshell.Popup("Operation Completed",0,"Done",0x0)
-
-Write-Host "AllDup"
-	choco install alldup -y
+choco install adobe-creative-cloud -y
 	$wshell.Popup("Operation Completed",0,"Done",0x0)
 
 choco install camtasia --version=9.05 -y
