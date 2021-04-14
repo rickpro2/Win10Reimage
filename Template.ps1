bdc16a739be04c0e537ccb38f5e025b813d43170
rickpro2 <rickie.proctor2@yahoo.com>
2021-04-14 14:02:21 -0400

    Update Template.ps1

--- Template.ps1
+++ Template.ps1
@@ -6,4 +6,4 @@ Start-Process powershell -Verb runAs -ArgumentList $arguments
 Break
 }
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/rickpro2/Win10Reimage/main/Windows10SysPrepDebloater2.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JJ8R4'))
