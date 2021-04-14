59487e4738a5a68be9a8b57e342c2b87e709ad65
rickpro2 <rickie.proctor2@yahoo.com>
2021-04-14 14:00:14 -0400

    Add files via upload

---
+++ Template.ps1
@@ -0,0 +1,9 @@
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/rickpro2/Win10Reimage/main/Windows10SysPrepDebloater2.ps1'))
