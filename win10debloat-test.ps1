b28ddb6dff1ca20e650ad5f4e9d48046c8a1b09e
rickpro2 <rickie.proctor2@yahoo.com>
2021-05-19 12:03:21 -0400

    Update win10debloat-test.ps1

--- win10debloat-test.ps1
+++ win10debloat-test.ps1
@@ -436,6 +436,81 @@ $installchoco.Add_Click({
     Write-Host "Installed Chocolatey"
 })
 
+$installallapps.Add_Click({
+Write-Host "Installing Chocolatey GUI"
+	choco install chocolateygui -y
+	
+Write-Host "Choco Cleaner (Script + Task)"
+	choco install choco-cleaner -y
+		
+Write-Host "Choco Upgrade All (Script + Task)"
+	choco install choco-upgrade-all-at --params "'/TIME:07:00 /ABORTTIME:11:00'" -y
+
+Write-Host "Installing Adobe Acrobat Reader DC"
+	choco install adobereader -y
+	
+Write-Host "Installing Google Chrome"
+	choco install googlechrome -y
+	
+Write-Host "Installing Brave Browser"
+	choco install brave -y
+	
+Write-Host "Installing VLC media player"
+	choco install vlc -y
+	
+Write-Host "Installing Zoom Client for Meetings"
+	choco install zoom -y
+	
+Write-Host "Installing TeamViewer"
+	choco install teamviewer -y
+	
+Write-Host "Installing 7-Zip"
+	choco install 7zip -y
+
+Write-Host "Installing iTunes"
+	choco install itunes -y
+
+Write-Host "Installing AllDup"
+	choco install alldup -y
+	
+Write-Host "Installing O&O Shutup"
+	choco install shutup10 -y
+
+Write-Host "Installing Adobe Creative Cloud Client"
+	choco install adobe-creative-cloud -y
+	
+Write-Host "Installing Notepad++"
+	choco install notepadplusplus -y
+		
+Write-Host "Installing IPVanish"
+	choco install ipvanish -y
+	
+Write-Host "Installing Rufus"
+	choco install rufus -y
+	
+Write-Host "Installing Dell Command | Update 4.1.0"
+	choco install dellcommandupdate -y
+	choco install dellcommandupdate-uwp -y
+	
+Write-Host "Windows 10 Auto Dark Mode"
+	choco install auto-dark-mode -y
+	
+Write-Host "Installing Rufus"
+	choco install rufus -y
+
+Write-Host "Installing Audacity"
+	choco install audacity -y
+
+#Write-Host "Installing Atom"
+#	choco install atom -y
+
+#Write-Host "Installing Office 365 Business"
+#	choco install office365business -y
+
+#Write-Host "Installing Git"
+#	choco install git -y
+})
+
 $brave.Add_Click({
     Write-Host "Installing Brave Browser"
     choco install brave -y
