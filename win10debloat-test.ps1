f6c83512fbc1aa45e732dcd632c1fa245d72723c
rickpro2 <rickie.proctor2@yahoo.com>
2021-05-21 19:28:17 -0400

    Update win10debloat-test.ps1

--- win10debloat-test.ps1
+++ win10debloat-test.ps1
@@ -51,12 +51,12 @@ $installchoco.height             = 70
 $installchoco.location           = New-Object System.Drawing.Point(16,19)
 $installchoco.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',16)
 
-$installchoco                    = New-Object system.Windows.Forms.Button
-$installchoco.text               = "Install All Apps"
-$installchoco.width              = 200
-$installchoco.height             = 70
-$installchoco.location           = New-Object System.Drawing.Point(90,19)
-$installchoco.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',16)
+$installallapps                    = New-Object system.Windows.Forms.Button
+$installallapps.text               = "Install All Apps"
+$installallapps.width              = 200
+$installallapps.height             = 70
+$installallapps.location           = New-Object System.Drawing.Point(90,19)
+$installallapps.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',16)
 
 $brave                           = New-Object system.Windows.Forms.Button
 $brave.text                      = "Brave Browser"
@@ -66,7 +66,7 @@ $brave.location                  = New-Object System.Drawing.Point(250,19)
 $brave.Font                      = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
 $Form.controls.AddRange(@($Panel1,$Label1,$Panel2,$Label3,$Panel3,$Label4,$Label15,$Panel4,$Label20,$Label21,$Label23,$PictureBox1))
-$Panel1.controls.AddRange(@($installchoco,$brave,$firefox,$7zip,$irfanview,$adobereader,$notepad,$gchrome,$mpc,$vlc,$powertoys,$winterminal,$vscode,$Label2))
+$Panel1.controls.AddRange(@($installchoco,$installallapps,$brave,$firefox,$7zip,$irfanview,$adobereader,$notepad,$gchrome,$mpc,$vlc,$powertoys,$winterminal,$vscode,$Label2))
 $Panel2.controls.AddRange(@($essentialtweaks,$backgroundapps,$cortana,$windowssearch,$actioncenter,$darkmode,$visualfx,$onedrive,$Label22,$lightmode))
 $Panel3.controls.AddRange(@($securitylow,$securityhigh,$Label5,$Label6,$Label7,$Label8,$Label9,$Label10,$Label11,$Label12,$Label13))
 $Panel4.controls.AddRange(@($defaultwindowsupdate,$securitywindowsupdate,$Label16,$Label17,$Label18,$Label19))
