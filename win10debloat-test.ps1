0c3e59f0f686a3fe30a028dc143e441a782ccb16
rickpro2 <rickie.proctor2@yahoo.com>
2021-10-13 11:18:20 -0400

    Update win10debloat-test.ps1

--- win10debloat-test.ps1
+++ win10debloat-test.ps1
@@ -33,7 +33,7 @@ $Form.TopMost                    = $false
 
 $Panel1                          = New-Object system.Windows.Forms.Panel
 $Panel1.height                   = 156
-$Panel1.width                    = 1032
+$Panel1.width                    = 1050
 $Panel1.location                 = New-Object System.Drawing.Point(9,90)
 
 $Label1                          = New-Object system.Windows.Forms.Label
