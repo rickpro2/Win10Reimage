3c90c2febc444c851886508dab13dc788f5da874
rickpro2 <rickie.proctor2@yahoo.com>
2021-10-15 02:06:27 -0400

    Update and rename Windows10SysPrepDebloater2.ps1 to Windows10SysPrepDebloater.ps1.ps1

--- gothrough/Windows10SysPrepDebloater2.ps1
+++
@@ -1,9 +0,0 @@
-If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-
-{   
-$arguments = "& '" + $myinvocation.mycommand.definition + "'"
-Start-Process powershell -Verb runAs -ArgumentList $arguments
-Break
-}
-
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JJ8R4'))
