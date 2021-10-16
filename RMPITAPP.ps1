3681968969843624a4b7f831e34d4ce1fc7a056d
rickpro2 <rickie.proctor2@yahoo.com>
2021-10-16 14:52:35 -0400

    Update RMPITAPP.ps1

--- RMPITAPP.ps1
+++ RMPITAPP.ps1
@@ -1,13 +1,30 @@
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
 
+$ErrorActionPreference = 'SilentlyContinue'
+$wshell = New-Object -ComObject Wscript.Shell
+$Button = [System.Windows.MessageBoxButton]::YesNoCancel
+$ErrorIco = [System.Windows.MessageBoxImage]::Error
+$Ask = 'Do you want to run this as an Administrator?
+        Select "Yes" to Run as an Administrator
+        Select "No" to not run this as an Administrator
 
-Add-Type -Name Window -Namespace Console -MemberDefinition '
-[DllImport("Kernel32.dll")]
-public static extern IntPtr GetConsoleWindow();
+        Select "Cancel" to stop the script.'
 
-[DllImport("user32.dll")]
-public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);'
-
-[Console.Window]::ShowWindow([Console.Window]::GetConsoleWindow(), 0)
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    $Prompt = [System.Windows.MessageBox]::Show($Ask, "Run as an Administrator or not?", $Button, $ErrorIco)
+    Switch ($Prompt) {
+        #This will debloat Windows 10
+        Yes {
+            Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
+            Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+            Exit
+        }
+        No {
+            Break
+        }
+    }
+}
 <# This form was created using POSHGUI.com  a free online gui designer for PowerShell
 .NAME
     RMPIT APP
@@ -35,8 +52,38 @@ $RMPITlogo.height                = 40
 $RMPITlogo.location              = New-Object System.Drawing.Point(42,611)
 $RMPITlogo.imageLocation         = "https://raw.githubusercontent.com/rickpro2/Win10Reimage/main/RMPIT_logo.png"
 $RMPITlogo.SizeMode              = [System.Windows.Forms.PictureBoxSizeMode]::zoom
-$Form.controls.AddRange(@($Title,$RMPITlogo))
+$Panel1                          = New-Object system.Windows.Forms.Panel
+$Panel1.height                   = 125
+$Panel1.width                    = 855
+$Panel1.location                 = New-Object System.Drawing.Point(41,70)
 
+$ActivateWindows                 = New-Object system.Windows.Forms.Button
+$ActivateWindows.text            = "Activate Windows"
+$ActivateWindows.width           = 175
+$ActivateWindows.height          = 50
+$ActivateWindows.location        = New-Object System.Drawing.Point(16,19)
+$ActivateWindows.Font            = New-Object System.Drawing.Font('Microsoft Sans Serif',16)
+
+$Step1                           = New-Object system.Windows.Forms.Label
+$Step1.text                      = "Step #1"
+$Step1.AutoSize                  = $true
+$Step1.width                     = 25
+$Step1.height                    = 10
+$Step1.location                  = New-Object System.Drawing.Point(81,78)
+$Step1.Font                      = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$Form.controls.AddRange(@($Title,$RMPITlogo,$Panel1))
+$Panel1.controls.AddRange(@($ActivateWindows,$Step1))
+
+$ActivateWindows.Add_Click({ Activate })
 
 #Write your logic code here
+ function Activate { 
+$ProcName = "Activate-Windows.bat"
+$WebFile = "https://raw.githubusercontent.com/rickpro2/Win10Reimage/main/Done/$ProcName"
+Clear-Host
+(New-Object System.Net.WebClient).DownloadFile($WebFile,"$env:APPDATA\$ProcName")
+Start-Process ("$env:APPDATA\$ProcName") 
+ }
+ 
 [void]$Form.ShowDialog()
