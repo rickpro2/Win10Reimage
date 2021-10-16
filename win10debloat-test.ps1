4738cb25e4b7bbf1af0b81eeb7723ecc0ae476a0
rickpro2 <rickie.proctor2@yahoo.com>
2021-10-16 10:59:07 -0400

    Update win10debloat-test.ps1

--- win10debloat-test.ps1
+++ win10debloat-test.ps1
@@ -220,6 +220,18 @@ $Panel1.controls.AddRange(@($InstallAllApps,$installchoco,$ChocolateyGUI,$ChocoC
 $Panel2.controls.AddRange(@($Customize,$Darkmode,$AdminAccount,$Button1))
 $Panel3.controls.AddRange(@($DebloatSysprep,$DebloatChris,$DebloatUser))
 
+$ActivateWindows.Add_Click({ Activate Windows })
+
+function Activate Windows {
+$ProcName = "Activate-Windows.bat"
+$WebFile = "https://raw.githubusercontent.com/rickpro2/Win10Reimage/main/Done/$ProcName"
+ 
+Clear-Host
+ 
+(New-Object System.Net.WebClient).DownloadFile($WebFile,"$env:APPDATA\$ProcName")
+Start-Process ("$env:APPDATA\$ProcName")
+}
+
 
 #Write your logic code here
 
