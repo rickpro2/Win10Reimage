4c83eff11ab57755062c02350fb388c8d0df07fe
rickpro2 <rickie.proctor2@yahoo.com>
2021-10-19 22:24:46 -0400

    Update sysprep.bat

--- Done/sysprep.bat
+++ Done/sysprep.bat
@@ -1,15 +1,3 @@
-:Sysprep
-REM **********************************************
-REM * SETUP SYSPREP AND RUN *
-REM **********************************************
-
-cls
-color f
-echo Getting everything ready for sysprep...
-echo Running sysprep...
-echo.
-timeout /t 7
-
 net user RMPIT ogoged /ADD /PASSWORDCHG:NO
 net localgroup administrators RMPIT /ADD
 
@@ -26,27 +14,3 @@ copy /Y Welcome.VBS %appdata%\Microsoft\Windows\Start Menu\Programs\Startup > NU
 
 
 C:\Windows\System32\sysprep\sysprep.exe /generalize /oobe /shutdown /unattend:C:\Windows\System32\Sysprep\unattend.xml
-
-
-REM We should never reach this line, it is here just to be on the safe side
-goto END
-
-:Reboot
-REM *********************************
-REM * REBOOT THE SYSTEM *
-REM *********************************
-
-cls
-color f
-echo Your Device is about to Reboot...
-echo.
-timeout /t 5
-
-REM Reboot the system
-SHUTDOWN /r /t 10 /c "Device will Restart in 10 Sec." /f
-
-REM *********************************
-REM * END OF BATCH FILE *
-REM *********************************
-
-:END
