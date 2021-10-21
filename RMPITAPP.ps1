c412eae48fb17dab99f8fedc5bb2545bde3e7f86
rickpro2 <rickie.proctor2@yahoo.com>
2021-10-21 11:36:35 -0400

    Update RMPITAPP.ps1

--- RMPITAPP.ps1
+++ RMPITAPP.ps1
@@ -54,7 +54,7 @@ $RMPITlogo.location              = New-Object System.Drawing.Point(42,611)
 $RMPITlogo.imageLocation         = "https://raw.githubusercontent.com/rickpro2/Win10Reimage/main/RMPIT_logo.png"
 $RMPITlogo.SizeMode              = [System.Windows.Forms.PictureBoxSizeMode]::zoom
 $Panel1                          = New-Object system.Windows.Forms.Panel
-$Panel1.height                   = 109
+$Panel1.height                   = 141
 $Panel1.width                    = 987
 $Panel1.location                 = New-Object System.Drawing.Point(31,71)
 
@@ -88,18 +88,6 @@ $Step2.height                    = 10
 $Step2.location                  = New-Object System.Drawing.Point(274,78)
 $Step2.Font                      = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$Panel2                          = New-Object system.Windows.Forms.Panel
-$Panel2.height                   = 150
-$Panel2.width                    = 300
-$Panel2.location                 = New-Object System.Drawing.Point(42,229)
-
-$Button1                         = New-Object system.Windows.Forms.Button
-$Button1.text                    = "button"
-$Button1.width                   = 130
-$Button1.height                  = 30
-$Button1.location                = New-Object System.Drawing.Point(151,97)
-$Button1.Font                    = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
-
 $Customiz                        = New-Object system.Windows.Forms.Button
 $Customiz.text                   = "Customize"
 $Customiz.width                  = 175
@@ -114,13 +102,6 @@ $Step3.width                     = 25
 $Step3.height                    = 10
 $Step3.location                  = New-Object System.Drawing.Point(467,78)
 $Step3.Font                      = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
-
-$installchoco                    = New-Object system.Windows.Forms.Button
-$installchoco.text               = "Install Chocolatey"
-$installchoco.width              = 175
-$installchoco.height             = 50
-$installchoco.location           = New-Object System.Drawing.Point(11,15)
-$installchoco.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',16)
 
 $Panel3                          = New-Object system.Windows.Forms.Panel
 $Panel3.height                   = 150
@@ -219,24 +200,30 @@ $Bloatware.height                = 30
 $Bloatware.location              = New-Object System.Drawing.Point(151,36)
 $Bloatware.Font                  = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$Form.controls.AddRange(@($Title,$RMPITlogo,$Panel1,$Panel2,$Panel3,$Panel4,$ResultText))
-$Panel1.controls.AddRange(@($ActivateWindows,$Step1,$ChocolateyAllApps,$Step2,$Customiz,$Step3,$Debloat,$Step4,$Sysprep,$Step5))
-$Panel2.controls.AddRange(@($Button1,$installchoco))
+$Debloat2                        = New-Object system.Windows.Forms.Button
+$Debloat2.text                   = "Debloat Sysprep"
+$Debloat2.width                  = 130
+$Debloat2.height                 = 30
+$Debloat2.location               = New-Object System.Drawing.Point(234,102)
+$Debloat2.Font                   = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$Form.controls.AddRange(@($Title,$RMPITlogo,$Panel1,$Panel3,$Panel4,$ResultText))
+$Panel1.controls.AddRange(@($ActivateWindows,$Step1,$ChocolateyAllApps,$Step2,$Customiz,$Step3,$Debloat,$Step4,$Sysprep,$Step5,$Debloat2))
 $Panel3.controls.AddRange(@($Button3,$Button4))
 $Panel4.controls.AddRange(@($Systeminfo,$Darkmode,$Label1,$Lightmode,$Bloatware))
 
 $ActivateWindows.Add_Click({ Activate })
 $ChocolateyAllApps.Add_Click({ Chocolateyapps })
 $Customiz.Add_Click({ customize })
-$installchoco.Add_Click({ ChocolateyInstall })
 $Debloat.Add_Click({ Debloat })
 $Systeminfo.Add_Click({ Systeminfo })
 $Sysprep.Add_Click({ Sysprep2 })
 $Darkmode.Add_Click({ darkmode })
 $Lightmode.Add_Click({ lightmode })
 $Bloatware.Add_Click({ removebolatware })
+$Debloat2.Add_Click({ DebloatSysprep })
 
-
+function DebloatSysprep { }
 #Write your logic code here
 function Activate { 
 $ProcName = "Activate-Windows.bat"
@@ -250,9 +237,9 @@ Start-Process ("$env:APPDATA\$ProcName")
 
 
 
-function Debloat {  
+function DebloatSysprep {  
 
-    #This function finds any AppX/AppXProvisioned package and uninstalls it, except for Freshpaint, Windows Calculator, Windows Store, and Windows Photos.
+#This function finds any AppX/AppXProvisioned package and uninstalls it, except for Freshpaint, Windows Calculator, Windows Store, and Windows Photos.
 #Also, to note - This does NOT remove essential system services/software/etc such as .NET framework installations, Cortana, Edge, etc.
 
 #This is the switch parameter for running this script as a 'silent' script, for use in MDT images or any type of mass deployment without user interaction.
@@ -513,7 +500,268 @@ Write-Output "Finished all tasks."
 }
 
 
+function DebloatSysprep { 
+    
+    
+    
+    
+    
+    
+    #This function finds any AppX/AppXProvisioned package and uninstalls it, except for Freshpaint, Windows Calculator, Windows Store, and Windows Photos.
+#Also, to note - This does NOT remove essential system services/software/etc such as .NET framework installations, Cortana, Edge, etc.
 
+#This is the switch parameter for running this script as a 'silent' script, for use in MDT images or any type of mass deployment without user interaction.
+
+param (
+  [switch]$Debloat, [switch]$SysPrep
+)
+
+    param([switch]$SysPrep)
+        Write-Verbose -Message ('Starting Sysprep Fixes')
+ 
+        # Disable Windows Store Automatic Updates
+       <# Write-Verbose -Message "Adding Registry key to Disable Windows Store Automatic Updates"
+        $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore"
+        If (!(Test-Path $registryPath)) {
+            Mkdir $registryPath -ErrorAction SilentlyContinue
+            New-ItemProperty $registryPath -Name AutoDownload -Value 2 
+        }
+        Else {
+            Set-ItemProperty $registryPath -Name AutoDownload -Value 2 
+        }
+        #Stop WindowsStore Installer Service and set to Disabled
+        Write-Verbose -Message ('Stopping InstallService')
+        Stop-Service InstallService 
+        #>
+ } 
+
+#Creates a PSDrive to be able to access the 'HKCR' tree
+New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+Function Start-Debloat {
+    
+    param([switch]$Debloat)
+
+    #Removes AppxPackages
+    #Credit to Reddit user /u/GavinEke for a modified version of my whitelist code
+    [regex]$WhitelistedApps = 'Microsoft.ScreenSketch|Microsoft.Paint3D|Microsoft.WindowsCalculator|Microsoft.WindowsStore|Microsoft.Windows.Photos|CanonicalGroupLimited.UbuntuonWindows|`
+    Microsoft.MicrosoftStickyNotes|Microsoft.MSPaint|Microsoft.WindowsCamera|.NET|Framework|Microsoft.HEIFImageExtension|Microsoft.ScreenSketch|Microsoft.StorePurchaseApp|`
+    Microsoft.VP9VideoExtensions|Microsoft.WebMediaExtensions|Microsoft.WebpImageExtension|Microsoft.DesktopAppInstaller'
+    Get-AppxPackage -AllUsers | Where-Object {$_.Name -NotMatch $WhitelistedApps} | Remove-AppxPackage -ErrorAction SilentlyContinue
+    # Run this again to avoid error on 1803 or having to reboot.
+    Get-AppxPackage -AllUsers | Where-Object {$_.Name -NotMatch $WhitelistedApps} | Remove-AppxPackage -ErrorAction SilentlyContinue
+    $AppxRemoval = Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -NotMatch $WhitelistedApps} 
+    ForEach ( $App in $AppxRemoval) {
+    
+        Remove-AppxProvisionedPackage -Online -PackageName $App.PackageName 
+        
+        }
+}
+
+Function Remove-Keys {
+        
+    Param([switch]$Debloat)    
+    
+    #These are the registry keys that it will delete.
+        
+    $Keys = @(
+        
+        #Remove Background Tasks
+        "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\46928bounde.EclipseManager_2.2.4.51_neutral__a5h4egax66k6y"
+        "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+        "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\Microsoft.MicrosoftOfficeHub_17.7909.7600.0_x64__8wekyb3d8bbwe"
+        "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\Microsoft.PPIProjection_10.0.15063.0_neutral_neutral_cw5n1h2txyewy"
+        "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\Microsoft.XboxGameCallableUI_1000.15063.0.0_neutral_neutral_cw5n1h2txyewy"
+        "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\Microsoft.XboxGameCallableUI_1000.16299.15.0_neutral_neutral_cw5n1h2txyewy"
+        
+        #Windows File
+        "HKCR:\Extensions\ContractId\Windows.File\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+        
+        #Registry keys to delete if they aren't uninstalled by RemoveAppXPackage/RemoveAppXProvisionedPackage
+        "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\46928bounde.EclipseManager_2.2.4.51_neutral__a5h4egax66k6y"
+        "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+        "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\Microsoft.PPIProjection_10.0.15063.0_neutral_neutral_cw5n1h2txyewy"
+        "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\Microsoft.XboxGameCallableUI_1000.15063.0.0_neutral_neutral_cw5n1h2txyewy"
+        "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\Microsoft.XboxGameCallableUI_1000.16299.15.0_neutral_neutral_cw5n1h2txyewy"
+        
+        #Scheduled Tasks to delete
+        "HKCR:\Extensions\ContractId\Windows.PreInstalledConfigTask\PackageId\Microsoft.MicrosoftOfficeHub_17.7909.7600.0_x64__8wekyb3d8bbwe"
+        
+        #Windows Protocol Keys
+        "HKCR:\Extensions\ContractId\Windows.Protocol\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+        "HKCR:\Extensions\ContractId\Windows.Protocol\PackageId\Microsoft.PPIProjection_10.0.15063.0_neutral_neutral_cw5n1h2txyewy"
+        "HKCR:\Extensions\ContractId\Windows.Protocol\PackageId\Microsoft.XboxGameCallableUI_1000.15063.0.0_neutral_neutral_cw5n1h2txyewy"
+        "HKCR:\Extensions\ContractId\Windows.Protocol\PackageId\Microsoft.XboxGameCallableUI_1000.16299.15.0_neutral_neutral_cw5n1h2txyewy"
+           
+        #Windows Share Target
+        "HKCR:\Extensions\ContractId\Windows.ShareTarget\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+    )
+    
+    #This writes the output of each key it is removing and also removes the keys listed above.
+    ForEach ($Key in $Keys) {
+        Write-Output "Removing $Key from registry"
+        Remove-Item $Key -Recurse -ErrorAction SilentlyContinue
+    }
+}
+        
+Function Protect-Privacy {
+    
+    Param([switch]$Debloat)    
+
+    #Creates a PSDrive to be able to access the 'HKCR' tree
+    New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+        
+    #Disables Windows Feedback Experience
+    Write-Output "Disabling Windows Feedback Experience program"
+    $Advertising = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo'
+    If (Test-Path $Advertising) {
+        Set-ItemProperty $Advertising -Name Enabled -Value 0 -Verbose
+    }
+        
+    #Stops Cortana from being used as part of your Windows Search Function
+    Write-Output "Stopping Cortana from being used as part of your Windows Search Function"
+    $Search = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search'
+    If (Test-Path $Search) {
+        Set-ItemProperty $Search -Name AllowCortana -Value 0 -Verbose
+    }
+        
+    #Stops the Windows Feedback Experience from sending anonymous data
+    Write-Output "Stopping the Windows Feedback Experience program"
+    $Period1 = 'HKCU:\Software\Microsoft\Siuf'
+    $Period2 = 'HKCU:\Software\Microsoft\Siuf\Rules'
+    $Period3 = 'HKCU:\Software\Microsoft\Siuf\Rules\PeriodInNanoSeconds'
+    If (!(Test-Path $Period3)) { 
+        mkdir $Period1 -ErrorAction SilentlyContinue
+        mkdir $Period2 -ErrorAction SilentlyContinue
+        mkdir $Period3 -ErrorAction SilentlyContinue
+        New-ItemProperty $Period3 -Name PeriodInNanoSeconds -Value 0 -Verbose -ErrorAction SilentlyContinue
+    }
+               
+    Write-Output "Adding Registry key to prevent bloatware apps from returning"
+    #Prevents bloatware applications from returning
+    $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
+    If (!(Test-Path $registryPath)) {
+        Mkdir $registryPath -ErrorAction SilentlyContinue
+        New-ItemProperty $registryPath -Name DisableWindowsConsumerFeatures -Value 1 -Verbose -ErrorAction SilentlyContinue
+    }          
+    
+    Write-Output "Setting Mixed Reality Portal value to 0 so that you can uninstall it in Settings"
+    $Holo = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Holographic'    
+    If (Test-Path $Holo) {
+        Set-ItemProperty $Holo -Name FirstRunSucceeded -Value 0 -Verbose
+    }
+    
+    #Disables live tiles
+    Write-Output "Disabling live tiles"
+    $Live = 'HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications'    
+    If (!(Test-Path $Live)) {
+        mkdir $Live -ErrorAction SilentlyContinue     
+        New-ItemProperty $Live -Name NoTileApplicationNotification -Value 1 -Verbose
+    }
+    
+    #Turns off Data Collection via the AllowTelemtry key by changing it to 0
+    Write-Output "Turning off Data Collection"
+    $DataCollection = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection'    
+    If (Test-Path $DataCollection) {
+        Set-ItemProperty $DataCollection -Name AllowTelemetry -Value 0 -Verbose
+    }
+    
+    #Disables People icon on Taskbar
+    Write-Output "Disabling People icon on Taskbar"
+    $People = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People'
+    If (Test-Path $People) {
+        Set-ItemProperty $People -Name PeopleBand -Value 0 -Verbose
+    }
+
+    #Disables suggestions on start menu
+    Write-Output "Disabling suggestions on the Start Menu"
+    $Suggestions = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager'    
+    If (Test-Path $Suggestions) {
+        Set-ItemProperty $Suggestions -Name SystemPaneSuggestionsEnabled -Value 0 -Verbose
+    }
+    
+    
+     Write-Output "Removing CloudStore from registry if it exists"
+     $CloudStore = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore'
+     If (Test-Path $CloudStore) {
+     Stop-Process -Name explorer -Force
+     Remove-Item $CloudStore -Recurse -Force
+     Start-Process Explorer.exe -Wait
+    }
+
+    #Loads the registry keys/values below into the NTUSER.DAT file which prevents the apps from redownloading. Credit to a60wattfish
+    reg load HKU\Default_User C:\Users\Default\NTUSER.DAT
+    Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name SystemPaneSuggestionsEnabled -Value 0
+    Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name PreInstalledAppsEnabled -Value 0
+    Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name OemPreInstalledAppsEnabled -Value 0
+    reg unload HKU\Default_User
+    
+    #Disables scheduled tasks that are considered unnecessary 
+    Write-Output "Disabling scheduled tasks"
+    #Get-ScheduledTask -TaskName XblGameSaveTaskLogon | Disable-ScheduledTask -ErrorAction SilentlyContinue
+    Get-ScheduledTask -TaskName XblGameSaveTask | Disable-ScheduledTask -ErrorAction SilentlyContinue
+    Get-ScheduledTask -TaskName Consolidator | Disable-ScheduledTask -ErrorAction SilentlyContinue
+    Get-ScheduledTask -TaskName UsbCeip | Disable-ScheduledTask -ErrorAction SilentlyContinue
+    Get-ScheduledTask -TaskName DmClient | Disable-ScheduledTask -ErrorAction SilentlyContinue
+    Get-ScheduledTask -TaskName DmClientOnScenarioDownload | Disable-ScheduledTask -ErrorAction SilentlyContinue
+}
+
+#This includes fixes by xsisbest
+Function FixWhitelistedApps {
+    
+    Param([switch]$Debloat)
+    
+    If(!(Get-AppxPackage -AllUsers | Select Microsoft.Paint3D, Microsoft.MSPaint, Microsoft.WindowsCalculator, Microsoft.WindowsStore, Microsoft.MicrosoftStickyNotes, Microsoft.WindowsSoundRecorder, Microsoft.Windows.Photos)) {
+    
+    #Credit to abulgatz for the 4 lines of code
+    Get-AppxPackage -allusers Microsoft.Paint3D | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+    Get-AppxPackage -allusers Microsoft.MSPaint | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+    Get-AppxPackage -allusers Microsoft.WindowsCalculator | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+    Get-AppxPackage -allusers Microsoft.WindowsStore | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+    Get-AppxPackage -allusers Microsoft.MicrosoftStickyNotes | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+    Get-AppxPackage -allusers Microsoft.WindowsSoundRecorder | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+    Get-AppxPackage -allusers Microsoft.Windows.Photos | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"} }
+}
+
+Function CheckDMWService {
+
+  Param([switch]$Debloat)
+  
+If (Get-Service -Name dmwappushservice | Where-Object {$_.StartType -eq "Disabled"}) {
+    Set-Service -Name dmwappushservice -StartupType Automatic}
+
+If(Get-Service -Name dmwappushservice | Where-Object {$_.Status -eq "Stopped"}) {
+   Start-Service -Name dmwappushservice} 
+  }
+
+Function CheckInstallService {
+  Param([switch]$Debloat)
+          If (Get-Service -Name InstallService | Where-Object {$_.Status -eq "Stopped"}) {  
+            Start-Service -Name InstallService
+            Set-Service -Name InstallService -StartupType Automatic 
+            }
+        }
+
+Write-Output "Initiating Sysprep"
+Begin-SysPrep
+Write-Output "Removing bloatware apps."
+Start-Debloat
+Write-Output "Removing leftover bloatware registry keys."
+Remove-Keys
+Write-Output "Checking to see if any Whitelisted Apps were removed, and if so re-adding them."
+FixWhitelistedApps
+Write-Output "Stopping telemetry, disabling unneccessary scheduled tasks, and preventing bloatware from returning."
+Protect-Privacy
+#Write-Output "Stopping Edge from taking over as the default PDF Viewer."
+#Stop-EdgePDF
+CheckDMWService
+CheckInstallService
+Write-Output "Finished all tasks."
+    
+    
+    
+    
+    
+}
 
 
 function Chocolateyapps {  
@@ -547,6 +795,11 @@ Clear-Host
 (New-Object System.Net.WebClient).DownloadFile($WebFile,"$env:APPDATA\$ProcName")
 Start-Process ("$env:APPDATA\$ProcName")
 }
+
+
+
+
+
 function Systeminfo { 
 $ProcName = "system info.bat"
 $WebFile = "https://raw.githubusercontent.com/rickpro2/Win10Reimage/main/Done/$ProcName"
@@ -667,7 +920,7 @@ function lightmode {
 )
 
 
-function removebolatware { 
+function Debloat { 
     Write-Host "Removing Bloatware"
 
     foreach ($Bloat in $Bloatware) {
