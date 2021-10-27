fde04d58fd9ea656eafd385f2574d8bf7d082b2f
rickpro2 <rickie.proctor2@yahoo.com>
2021-10-27 12:32:17 -0400

    Update Online KMS Activation Script v6.0.cmd

--- gothrough/Online KMS Activation Script v6.0.cmd
+++ gothrough/Online KMS Activation Script v6.0.cmd
@@ -1,6 +1,32 @@
+EM --add the following to the top of your bat file--
+
+
 @echo off
 
-if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
 
 
 
