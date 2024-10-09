d34610486c5e4c47ab1bde0d83c3013390f42602
Rickie M. Proctor II <rickie.proctor2@yahoo.com>
2024-10-08 21:57:39 -0400

    Update Chocolatey-Apps-Install.bat

--- scripts/Chocolatey-Apps-Install.bat
+++ scripts/Chocolatey-Apps-Install.bat
@@ -38,23 +38,68 @@ choco feature enable -n=allowGlobalConfirmation
 
 Write-Host "Installing Chocolatey GUI"
 $ResultText.text = "`r`n" +"`r`n" + "Installing Chocolatey GUI... Please Wait"
-choco install chocolateygui -y
+choco install chocolateygui --version 2.1.1 -y
 $ResultText.text = "`r`n" + "Finished Installing Chocolatey GUI" + "`r`n" + "`r`n" + "Ready for Next Task"
 
 Write-Host "Installing Choco Cleaner (Script + Task)"
 $ResultText.text = "`r`n" +"`r`n" + "Installing Choco Cleaner (Script + Task)... Please Wait"
-choco install choco-cleaner -y
+choco install choco-cleaner --version 1.1.1 -y
 $ResultText.text = "`r`n" + "Finished Installing Choco Cleaner (Script + Task)" + "`r`n" + "`r`n" + "Ready for Next Task"
 
 Write-Host "Installing Choco Upgrade All (Script + Task)"
 $ResultText.text = "`r`n" +"`r`n" + "Installing Choco Upgrade All (Script + Task)... Please Wait"
-choco install choco-upgrade-all-at-startup -y
+choco install choco-upgrade-all-at-startup --version 2023.5.26 -y
 $ResultText.text = "`r`n" + "Finished Installing Choco Upgrade All (Script + Task)" + "`r`n" + "`r`n" + "Ready for Next Task"
 
 Write-Host "Installing Adobe Acrobat Reader DC"
 $ResultText.text = "`r`n" +"`r`n" + "Installing Adobe Acrobat Reader DC... Please Wait"
-choco install adobereader -y
+choco install adobereader --version 2024.3.20112 -y
 $ResultText.text = "`r`n" + "Finished Installing Adobe Acrobat Reader DC" + "`r`n" + "`r`n" + "Ready for Next Task"
+
+
+
+
+
+
+
+
+
+
+
+
+
+choco install googlechrome --version 129.0.6668.90 -y
+choco install vlc --version 3.0.21 -y
+choco install zoom --version 6.2.3.47507 -y
+choco install rustdesk --version 1.2.6 -y
+choco install notepadplusplus --version 8.7.0 -y
+choco install malwarebytes --version 5.2.0.112 -y
+choco install open-shell --version 4.4.191 -y
+choco install teamviewer --version 15.58.5 -y
+choco install 7zip --version 24.8.0 -y
+choco install alldup --version 4.5.62 -y
+choco install rufus --version 4.5.0 -y
+choco install etcher --version 1.19.21 -y
+choco install putty --version 0.81.0 -y
+choco install filezilla --version 3.67.1 -y
+choco install handbrake --version 1.8.2 -y
+choco install supportassist --version 3.14.2.45116 -y
+choco install googledrive --version 97.0.1 -y
+choco install office365business --version 17830.20162 -y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 Write-Host "Installing Google Chrome"
 $ResultText.text = "`r`n" +"`r`n" + "Installing Google Chrome... Please Wait"
@@ -108,7 +153,7 @@ $ResultText.text = "`r`n" + "Finished Installing 7-Zip" + "`r`n" + "`r`n" + "Rea
 
 Write-Host "Installing iTunes"
 $ResultText.text = "`r`n" +"`r`n" + "Installing iTunes... Please Wait"
-choco install iTunes -y
+choco install itunes -y
 $ResultText.text = "`r`n" + "Finished Installing iTunes" + "`r`n" + "`r`n" + "Ready for Next Task"
 
 rem  Write-Host "Installing AllDup"
@@ -196,10 +241,10 @@ rem  $ResultText.text = "`r`n" +"`r`n" + "Installing Audacity... Please Wait"
 rem  choco install audacity -y
 rem  $ResultText.text = "`r`n" + "Finished Installing Audacity" + "`r`n" + "`r`n" + "Ready for Next Task"  
 
-rem Write-Host "Installing Google Drive for Desktop"
-rem $ResultText.text = "`r`n" +"`r`n" + "Installing Google Drive for Desktop... Please Wait"
-rem choco install google-drive-file-stream -y
-rem $ResultText.text = "`r`n" + "Finished Installing Google Drive for Desktop" + "`r`n" + "`r`n" + "Ready for Next Task"  
+Write-Host "Installing Google Drive for Desktop"
+$ResultText.text = "`r`n" +"`r`n" + "Installing Google Drive for Desktop... Please Wait"
+choco install google-drive-file-stream -y
+$ResultText.text = "`r`n" + "Finished Installing Google Drive for Desktop" + "`r`n" + "`r`n" + "Ready for Next Task"  
 
 rem Write-Host "Installing Winaero Tweaker"
 rem $ResultText.text = "`r`n" +"`r`n" + "Installing Winaero Tweaker... Please Wait"
