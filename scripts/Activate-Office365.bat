b6d894f83d05e57245fa14f4cd763c13cecdc0a9
Rickie M. Proctor II <rickie.proctor2@yahoo.com>
2024-10-09 19:01:41 -0400

    Update Activate-Office365.bat

--- scripts/Activate-Office365.bat
+++ scripts/Activate-Office365.bat
@@ -1,1 +1,124 @@
-[@echo off title Activate Office 365 ProPlus for FREE - MSGuides.com&cls&echo =====================================================================================&echo #Project: Activating Microsoft software products for FREE without additional software&echo =====================================================================================&echo.&echo #Supported products: Office 365 ProPlus (x86-x64)&echo.&echo.&(if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")&(if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16")&(for /f %%x in ('dir /b ..\root\Licenses16\proplusvl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16%%x" >nul)&(for /f %%x in ('dir /b ..\root\Licenses16\proplusvl_mak*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16%%x" >nul)&echo.&echo ============================================================================&echo Activating your Office...&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo ospp.vbs /setprt:1688 >nul&cscript //nologo ospp.vbs /unpkey:WFG99 >nul&cscript //nologo ospp.vbs /unpkey:DRTFM >nul&cscript //nologo ospp.vbs /unpkey:BTDRB >nul&set i=1&cscript //nologo ospp.vbs /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99 >nul||cscript //nologo ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP >nul||goto notsupported :skms if %i% GTR 10 goto busy if %i% EQU 1 set KMS=kms7.MSGuides.com if %i% EQU 2 set KMS=s8.uk.to if %i% EQU 3 set KMS=s9.us.to if %i% GTR 3 goto ato cscript //nologo ospp.vbs /sethst:%KMS% >nul :ato echo ============================================================================&echo.&echo.&cscript //nologo ospp.vbs /act | find /i "successful" && (echo.&echo ============================================================================&echo.&echo #My official blog: MSGuides.com&echo.&echo #How it works: bit.ly/kms-server&echo.&echo #Please feel free to contact me at msguides.com@gmail.com if you have any questions or concerns.&echo.&echo #Please consider supporting this project: donate.msguides.com&echo #Your support is helping me keep my servers running 24/7!&echo.&echo ============================================================================&choice /n /c YN /m "Would you like to visit my blog [Y,N]?" & if errorlevel 2 exit) || (echo The connection to my KMS server failed! Trying to connect to another one... & echo Please wait... & echo. & echo. & set /a i+=1 & goto skms) explorer "http://MSGuides.com"&goto halt :notsupported echo ============================================================================&echo.&echo Sorry, your version is not supported.&echo.&goto halt :busy echo ============================================================================&echo.&echo Sorry, the server is busy and can't respond to your request. Please try again.&echo. :halt pause >nul]
+mode con: cols=115 lines=18
+title Office 365 Activator by Muki
+@echo off
+chcp 65001 > nul
+cls
+color 3
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto menu
+) else (
+    goto administatorError
+)
+
+
+:menu
+cls
+echo.
+echo      ____    __   __  _               ____     __  _____                    _    _               _
+echo "   / __ \  / _| / _|(_)             |___ \   / / | ____|       /\         | |  (_)             | |
+echo "  | |  | || |_ | |_  _   ___  ___     __) | / /_ | |__        /  \    ___ | |_  _ __   __ __ _ | |_  ___   _ __
+echo "  | |  | ||  _||  _|| | / __|/ _ \   |__ < | '_ \|___ \      / /\ \  / __|| __|| |\ \ / // _` || __|/ _ \ | '__|
+echo "  | |__| || |  | |  | || (__|  __/   ___) || (_)| ___) |    / ____ \| (__ | |_ | | \ V /| (_| || |_| (_) || |
+echo "   \____/ |_|  |_|  |_| \___|\___|  |____/  \___/|____/    /_/    \_\\___| \__||_|  \_/  \__,_| \__|\___/ |_|
+echo      Created by Muki                                                                         github.com/muki01
+echo.
+echo.
+echo                                             1. Activate Office 365
+echo.
+echo.
+echo.
+set /p select=">>> "
+if %select%==1 goto applyKey
+
+
+
+:applyKey
+    cls
+    echo.
+    echo ------------------------
+    echo   Activation Started !
+    echo   Please wait...
+    echo ------------------------
+    echo.
+    if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" (
+        cd /d "%ProgramFiles%\Microsoft Office\Office16"
+    ) else if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" (
+        cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16"
+    )
+    for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do (
+        cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul
+    )
+    cscript //nologo ospp.vbs /setprt:1688 >nul
+    cscript //nologo ospp.vbs /unpkey:6F7TH >nul
+    cscript //nologo ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH >nul
+    set server=1
+    goto skms
+
+
+:skms
+    if %server% == 1 set KMS=107.175.77.7
+    if %server% == 2 set KMS=kms7.devomman.com
+    if %server% == 3 set KMS=e8.us.to
+    if %server% == 4 set KMS=e9.us.to
+    if %server% == 5 set KMS=kms7.MSGuides.com
+    if %server% == 6 goto busyError
+    cscript //nologo ospp.vbs /sethst:%KMS% >nul
+    goto activate
+
+
+:activate
+    cscript //nologo ospp.vbs /act | find /i "successful" && (
+        goto activatedSuccessfully
+    ) || (
+        goto serverError
+    )
+
+
+
+:activatedSuccessfully
+    cls
+    echo.
+    echo ---------------------------------
+    echo   Office Activated Successfully
+    echo ---------------------------------
+    echo.
+    ping localhost -n 5 >nul
+    goto menu
+
+
+:serverError
+    cls
+    echo.
+    echo ------------------------------------------
+    echo   The connection to the server failed! 
+    echo   Trying to connect to another server...
+    echo   Please wait...
+    echo ------------------------------------------
+    echo.
+    set /a server+=1
+    ping localhost -n 5 >nul
+    goto skms
+
+:busyError
+    cls
+    echo.
+    echo ----------------------------------------------------------------
+    echo   Sorry, the server is busy and can't respond to your request. 
+    echo   Please try again.
+    echo ----------------------------------------------------------------
+    echo.
+    ping localhost -n 5 >nul
+    goto menu
+
+
+:administatorError
+    cls
+    echo.
+    echo ---------------------------------------
+    echo   Run this Program As Administrator !
+    echo ---------------------------------------
+    echo.
+    pause
+    exit
